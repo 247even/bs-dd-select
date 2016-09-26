abcf0dc7ce40bf50e77175c0ba700eedf96e1edf
247even <247even@users.noreply.github.com>
2016-09-26 22:09:39 +0200

    Update bs-dd-select.js

--- bs-dd-select.js
+++ bs-dd-select.js
@@ -1,4 +1,8 @@
-$(function() {
+var ddSelect = function(cb){
+	
+	var val;
+	this.value = val;
+	
 	$('.bs-dd-select li').on('click', function(){
 		$(this).parent().find('li').removeClass('bs-dd-selected hidden');
 		$(this).addClass('bs-dd-selected hidden');
@@ -6,9 +10,22 @@ $(function() {
 	
 	$('.bs-dd-select').on('hidden.bs.dropdown', function(e) {
 		var sel = $(this).find('.bs-dd-selected');
-		var val = sel.attr('data-value');
+		val = sel.attr('data-value');
 		var caret = ' <span class="caret"></span>';
 		var txt = sel.find('a').html();
 		$(this).find('button').html(txt+caret);
-	});
+		if(cb){
+			cb(val);
+		}
+	});	
+};
+
+$(function() {
+	//ddSelect();
 });
+
+/*
+ddSelect(function(val){
+  console.log(val);
+});
+*/
